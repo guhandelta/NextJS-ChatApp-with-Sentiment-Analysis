a449bd52d79a8471d06025a0f2d09a350be9e792
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-05-27 22:07:18 -0400

    Added the logic to check the latest message and assign the message with an appropriate smiley, as per the sentiment score

--- components/Chat.js
+++ components/Chat.js
@@ -2,6 +2,12 @@ import React, { Fragment } from 'react';
 import axios from 'axios';
 import Pusher from 'pusher-js';
 
+import ChatMessages from './ChatMessages';
+
+const SAD_EMOJI = [55357, 56864];
+const HAPPY_EMOJI = [55357, 56832];
+const NEUTRAL_EMOJI = [55357, 56848];
+
 class Chat extends React.Component {
 
     state = {
@@ -62,6 +68,44 @@ class Chat extends React.Component {
             </div>
 
             <div className="border-top border-gray w-100 px-4 d-flex align-items-center bg-light" style={{ minHeight: 90 }}>
+                {this.state.chats.map((chat, index) => {  // Iterate through the chatsarray propertities, in the state, to check if...
+                    //... the sender of the message is the same as the currently active user and use that to determine the position of the...
+                    //... displayed chat message
+
+                    const previous = Math.max(0, index - 1);
+                    const previousChat = this.state.chats[previous];
+                    const position = chat.user === this.props.activeUser ? "right" : "left";
+
+                    // Current chat message is the first in the list
+                    const isFirst = previous === index;
+                    // Current chat message directly follows a message from another user
+                    const inSequence = chat.user === previousChat.user;
+                    // Current chat message has a delay of over 1 minute from the previous message of the same user
+                    const hasDelay = Math.ceil((chat.timestamp - previousChat.timestamp) / (1000 * 60)) > 1;
+                    // Set the mood of the user while typing the message to either happy, sad or neutral using the earlier defined constants...
+                    //...using the sentiment score in the chat object
+                    const mood = chat.sentiment > 0 ? HAPPY_EMOJI : (chat.sentiment === 0 ? NEUTRAL_EMOJI : SAD_EMOJI);
+
+                    return (
+                        // Global_Objects/String/fromCodePoint
+                        // String.fromCodePoint(...mood) => fn() added in ES6 to get the emoji from the code points defined in constants earlier
+                        <Fragment key={index}>
+
+                            {(isFirst || !inSequence || hasDelay) && (
+                                <div className={`d-block w-100 font-weight-bold text-dark mt-4 pb-1 px-1 text-${position}`} style={{ fontSize: '0.9rem' }}>
+                                    <span className="d-block" style={{ fontSize: '1.6rem' }}>
+                                        {String.fromCodePoint(...mood)}
+                                    </span>
+                                    <span>{chat.user || 'Anonymous'}</span>
+                                </div>
+                            )}
+
+                            <ChatMessages message={chat.message} position={position} />
+
+                        </Fragment>
+                    );
+
+                })}
                 <textarea className="form-control px-3 py-2" onKeyUp={this.handleKeyUp} placeholder="Enter a chat message" style={{ resize: 'none' }}></textarea>
             </div>
 
