be58a87e409af96269fc22c6ec561f24fce0439e
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-05-27 21:02:46 -0400

    Added endpoints to the server for saving and retrieving the chat history

--- server.js
+++ server.js
@@ -37,6 +37,31 @@ app.prepare()
             return handler(req, res);
         });
 
+        // Create an in-memory store for the chat history, to store chat messages in an array. This is useful for new clients that join...
+        //... the chat room to see previous messages. Whenever the Pusher client makes a POST request to the /messages endpoint on...
+        //... connection, it gets all the messages in the chat history in the returned response.
+        const chatHistory = { messages: [] };
+
+        server.post('/message', (req, res, next) => {
+            // Fetch the chat payload from req.body through the help of the body-parsermiddleware
+            const { user = null, message = '', timestamp = +new Date } = req.body;
+            // Calculate the overall sentiment score of the chat message using the sentiment module
+            const sentimentScore = sentiment.analyze(message).score;
+
+            // Reconstruct the chat object, adding the sentiment property containing the sentiment score
+            const chat = { user, message, timestamp, sentiment: sentimentScore };
+            // Add the chat to the chat history messages
+            chatHistory.messages.push(chat);
+            // Trigger a new-message event on the chat-room Pusher channel, passing the chat object in the event data
+            pusher.trigger('chat-room', 'new-message', { chat });
+        });
+
+        //Whenever the Pusher client makes a POST request to the /messages endpoint on connection, it gets all the messages...
+        //... in the chat history in the returned response
+        server.post('/messages', (req, res, next) => {
+            res.json({ ...chatHistory, status: 'success' });
+        });
+
         server.listen(port, err => {
             if (err) throw err;
             console.log(`> Ready on http://localhost:${port}`);
